e95f19e4a538a58e65b6e6d1ae9fe43c392a9423
tornike-MACS <tshub23@freeuni.edu.ge>
2026-06-06 17:44:57 +0400

    Deploy ConfidentialPayroll to Sepolia

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployed = await deploy("ConfidentialPayroll", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`ConfidentialPayroll contract: `, deployed.address);
 };
+
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_confidentialPayroll"; // id required to prevent reexecution
+func.tags = ["ConfidentialPayroll"];
